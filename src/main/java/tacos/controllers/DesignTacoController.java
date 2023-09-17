775b330dd17dacf4e5587c52a1e62e214a980c03
thanhnd <ducthanhnguyen95@gmail.com>
2023-09-17 07:20:15 +0700

    Developing web applications

--- src/main/java/tacos/controllers/DesignTacoController.java
+++ src/main/java/tacos/controllers/DesignTacoController.java
@@ -1,4 +1,4 @@
-package tacos.web;
+package tacos.controllers;
 
 import java.util.Arrays;
 import java.util.List;
@@ -6,16 +6,19 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import tacos.Ingredient;
-import tacos.Ingredient.Type;
-import tacos.Taco;
-import tacos.TacoOrder;
+import tacos.entities.Ingredient;
+import tacos.entities.Ingredient.Type;
+import tacos.entities.Taco;
+import tacos.entities.TacoOrder;
 
 @Slf4j
 @Controller
@@ -50,6 +53,16 @@ public class DesignTacoController {
 	@GetMapping
 	public String showDesignForm() {
 		return "design";
+	}
+
+	@PostMapping
+	public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
+		tacoOrder.addTaco(taco);
+		log.info("Processing taco: {}", taco);
+		return "redirect:/orders/current";
 	}
 
 	private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
--- src/main/java/tacos/entities/Ingredient.java
+++ src/main/java/tacos/entities/Ingredient.java
@@ -1,4 +1,4 @@
-package tacos;
+package tacos.entities;
 
 import lombok.Data;
 
